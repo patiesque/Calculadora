baec416b309d353103ac2909daca5a7c7758f686
Patricia Matiesque <patymatiesque@gmail.com>
2020-05-28 21:59:55 -0300

    soma

--- calc.test.js
+++ calc.test.js
@@ -2,4 +2,11 @@ const calculadora = require("./calc");
 
 describe("calculadora", () => {
     
+    describe("soma", () => {
+        it("retorna 3 quando 2 somado com 1", () => {
+            const num1 = 2;
+            const num2 = 1;
+            expect(calculadora.soma(num1, num2)).toBe(3);
+        })
+    })
 })
