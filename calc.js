c63263cb16894cda3de16adf87a9a68879208870
Patricia Matiesque <patymatiesque@gmail.com>
2020-05-29 14:12:07 -0300

    multiplicação

--- calc.js
+++ calc.js
@@ -6,7 +6,21 @@ function subtracao(num1, num2) {
     return num1 - num2
 }
 
+function multiplicacao(num1, num2){
+    return num1 * num2
+}
+
+function divisao(num1, num2) {
+    if(num2 === 0){
+        return "Erro, não pode dividir por 0"
+    }
+
+    return num1 / num2
+}
+
 module.exports = {
     soma,
-    subtracao
+    subtracao, 
+    multiplicacao,
+    divisao
 }
--- calc.test.js
+++ calc.test.js
@@ -1,7 +1,7 @@
 const calculadora = require("./calc");
 
 describe("calculadora", () => {
-    
+
     describe("soma", () => {
         it("retorna 3 quando 2 somado com 1", () => {
             const num1 = 2;
@@ -17,4 +17,28 @@ describe("calculadora", () => {
             expect(calculadora.subtracao(num1, num2)).toBe(1)
         })
     })
+
+    describe("multiplição", () => {
+        it("retorna 6 quando 2 multiplicado por 3", () => {
+            const num1 = 2;
+            const num2 = 3;
+            expect(calculadora.multiplicacao(num1, num2)).toBe(6)
+        })
+    })
+
+    describe("divisão", () => {
+        it("retorna 2 quando 6 divido por 3", () => {
+            const num1 = 6;
+            const num2 = 3;
+            expect(calculadora.divisao(num1, num2)).toBe(2)
+        })
+
+        it("retorna Erro ao dividir por 0", () => {
+            const num1 = 6;
+            const num2 = 0;
+            expect(calculadora.divisao(num1, num2)).toBe(
+                "Erro, não pode dividir por 0")
+        })
+    })
+
 })
